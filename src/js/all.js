20c00b1c275fe9fabcdda0267d2d7df4faeac43e
Tuan Anh <tuananhtybg.99@gmail.com>
2022-07-20 16:33:29 +0700

    Git commit

---
+++ src/js/all.js
@@ -0,0 +1,12 @@
+
+var menu = document.getElementById("menu");
+
+function toggleMenu() {
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+}
+
+
