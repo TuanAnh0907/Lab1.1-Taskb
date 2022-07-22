480de657af02b767ba191cbf01b9dde2031213ea
Tuan Anh <tuananhtybg.99@gmail.com>
2022-07-22 17:31:57 +0700

    Commit 1

--- src/js/all.js
+++ src/js/all.js
@@ -1,12 +1,30 @@
 
-var menu = document.getElementById("menu");
+// var menu = document.getElementById("menu");
 
-function toggleMenu() {
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
-    }
-}
+// function toggleMenu() {
+//     if (menu.classList.contains('hidden')) {
+//         menu.classList.remove('hidden');
+//     } else {
+//         menu.classList.add('hidden');
+//     }
+// }
 
 
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
